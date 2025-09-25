a16ef8688c13ee0e108192a2a9506dc4e9fae947
megumi <makhmudova.vena@gmail.com>
2025-09-25 21:44:26 +0400

    Initial commit

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows;
+
+namespace WpfApp5
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<ProcessData> Processes { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Processes = new ObservableCollection<ProcessData>();
+            ProcessesGrid.ItemsSource = Processes;
+            RefreshProcesses();
+        }
+
+        private void RefreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshProcesses();
+        }
+
+        private void KillBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProcessesGrid.SelectedItem is ProcessData selectedProcess)
+            {
+                try
+                {
+                    var process = Process.GetProcessById(selectedProcess.Id);
+                    process.Kill();
+                    RefreshProcesses();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void RefreshProcesses()
+        {
+            Processes.Clear();
+            
+            try
+            {
+                var processes = Process.GetProcesses();
+                foreach (var process in processes)
+                {
+                    try
+                    {
+                        Processes.Add(new ProcessData
+                        {
+                            Id = process.Id,
+                            Name = process.ProcessName,
+                        });
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show($"Error:");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ WpfApp5/ProcessData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WpfApp5
+{
+    public class ProcessData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        
+    }
+}
